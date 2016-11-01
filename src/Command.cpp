4497748939530b85222eaa551609b8c068a1479e
Calvin Ta <tacalvin123@gmail.com>
2016-11-01 11:50:31 -0700

    added exit and cs

--- src/Command.cpp
+++ src/Command.cpp
@@ -4,15 +4,38 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <string.h>
 using namespace std;
 Command::Command(vector<char*> s)
 {
   cmd = s;
 }
 
+int cd(char* args)
+{
+	int condition = chdir(args);
+	return condition++;
+}
+
+void exitf()
+{
+	exit(0);
+}
+
+
 // Will return false if no erros and true if there are errors
 bool Command::evaluate()
 {
+  if(!strcmp(cmd.at(0),"exit")) 
+  {
+	  exitf();
+	  //return true;
+  }
+  if(!strcmp(cmd.at(0),"cd")) 
+  {
+	  int condition = chdir(cmd.at(1));
+	  return !condition;
+  }
   pid_t pid, wpid;
   int status;
   //execvp needs a null terminated char **
@@ -20,9 +43,7 @@ bool Command::evaluate()
   //using internal vector sturcture
   char** args = &cmd[0];
   pid = fork();
-
-
-
+	
 
   if (pid == 0)
   {
